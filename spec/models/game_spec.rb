f662b14b6f333f14db5e3232c296cf2379a69823
Yaroslav <kyzon42@gmail.com>
2017-07-25 00:00:34 +0300

    [models][migrations] create User and Game

---
+++ spec/models/game_spec.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: games
+#
+#  id             :integer          not null, primary key
+#  tourney_number :integer
+#  players        :integer
+#  buy_in         :integer
+#  rake           :integer
+#  prize_pool     :integer
+#  place          :integer
+#  reward         :integer          default(0)
+#  started_at     :datetime
+#  user_id        :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+# Indexes
+#
+#  index_games_on_user_id  (user_id)
+#
+
+require 'rails_helper'
+
+RSpec.describe Game, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
