7e1bac0fd47255bd99923201c629f586a7e5ff3c
Yaroslav <kyzon42@gmail.com>
2017-08-08 23:57:55 +0300

    [parser] first uncomplete version of parser

--- app/services/tournament/parser.rb
+++ app/services/tournament/parser.rb
@@ -1,12 +1,63 @@
 module Tournament
   class Parser
+    attr_accessor :result
+    attr_accessor :nickname, :file_path
+
+    def initialize(nickname, file_path)
+      self.nickname = nickname
+      self.file_path = file_path
+      self.result = {}
+    end
+
     def call
-      File.open(file_path) do |f|
+      File.open(file_path).each.with_index do |line, i|
+        case i
+        when 0 then tourney_number(line)
+        when 1 then buyin_and_rake(line)
+        when 2 then players(line)
+        when 3 then prize_pool(line)
+        when 4 then started_at(line)
+        # else
+        #   find_user_place_and_reward(line)
+        end
         # TODO: write parsing
         # there will be parsing function code
         # result will save to data hash
         # may occur ParsingError
       end
+      result.symbolize_keys!
     end
+
+    private
+
+    def tourney_number(line)
+      match_result = line.match(/№(?<tourney_number>\d+)/).named_captures
+      result.merge!(match_result)
+    end
+
+    def buyin_and_rake(line)
+      match_result = line.match(/\$(?<buy_in>\d+\.\d+)\/\$(?<rake>\d+\.\d+)/).named_captures
+      result.merge!(match_result)
+    end
+
+    def players(line)
+      match_result = line.match(/(?<players>\d+)/).named_captures
+      result.merge!(match_result)
+    end
+
+    def prize_pool(line)
+      match_result = line.match(/\$(?<prize_pool>\d+\.\d+)/).named_captures
+      result.merge!(match_result)
+    end
+
+    def started_at(line)
+      match_result = line.match(/\[(?<started_at>.*)\]/).named_captures
+      result.merge!(match_result)
+    end
+
+    # def find_user_place_and_reward(line)
+    #   match_result = line.match(/(?<place>\d+): (?<nickname>\w+) .+\), \$(?<reward>\d+\,\d+)/).named_captures
+    #   result.merge!(match_result) if match_result[:nickname] == nickname
+    # end
   end
 end
---
+++ spec/services/tournament/parser_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Tournament::Parser do
+  describe 'valid file' do
+    let(:nickname) { 'Fedor' }
+    let(:valid_file_path) { Rails.root.join('spec', 'fixtures', 'valid_tourney.txt') }
+    subject { Tournament::Parser.new(nickname, valid_file_path).call }
+
+    it 'return correct hash' do
+      expect(subject).to include(
+        tourney_number: '100500', # 100500
+        players: '3', # 3
+        buy_in: '0.92', # 92
+        rake: '0.08', # 8
+        prize_pool: '2.00', # 200
+        # place: '1', # 1
+        # reward: '2,00', # 200
+        started_at: '01.01.2017 16:50:55 ВВ' # DateTime.parse('01.01.2017 16:50:55 ВВ')
+      )
+    end
+  end
+
+  describe 'invalid file' do
+    pending 'spec in progress'
+  end
+end
