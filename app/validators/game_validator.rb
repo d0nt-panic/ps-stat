d202fc1170425418c9b19cebbfafcf47fd54ff41
Yaroslav <kyzon42@gmail.com>
2017-08-18 01:23:26 +0300

    [validator] game validator specs complete

--- app/validators/game_validator.rb
+++ app/validators/game_validator.rb
@@ -1,23 +1,28 @@
 class GameValidator < Dry::Struct
   attribute :data, Types::Strict::Hash
 
+  attr_accessor :errors
+
   def save!
     result = ValidationSchema.call(data)
-    return nil unless result.success? && data.delete(:nickname) == nickname
+    unless result.success?
+      self.errors = result.errors
+      return nil
+    end
 
     game = Game.create(data)
     game
   end
 
   ValidationSchema = Dry::Validation.Form do
-    required(:tourn_summaries).filled(:int?)
+    required(:tourney_number).filled(:int?)
     required(:players).filled(:int?)
     required(:buy_in).filled(:int?)
     required(:rake).filled(:int?)
     required(:prize_pool).filled(:int?)
     required(:place).filled(:int?)
     required(:reward).filled(:int?)
-    required(:started_at).filled(:date?)
+    required(:started_at).filled(:time?)
 
     required(:user_id).filled(:int?)
     required(:tourn_summary_id).filled(:int?)
